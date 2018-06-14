c83cea97efdffcc0d640de4749d3bb5ab0c9d461
agokhale11 <39194493+agokhale11@users.noreply.github.com>
2018-06-14 15:11:31 -0500

    updated settings

--- main/wsgi.py
+++ main/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tfx_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tfx_project.main.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
