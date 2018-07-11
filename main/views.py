2feb359cd296fbe1d59e43a915abf2a104ac861c
agokhale11 <39194493+agokhale11@users.noreply.github.com>
2018-07-11 14:02:32 -0500

    logic error

--- main/views.py
+++ main/views.py
@@ -833,7 +833,7 @@ def assign_teams_view(request, spaceurl):
                 max_project = name
 
         team_project = Project.objects.get(name=max_project)
-        if TeamProject.objects.filter(space=space, team=team, project=team_project):
+        if not TeamProject.objects.filter(space=space, team=team, project=team_project):
             new_project_team = TeamProject(space=space, team=team, project=team_project)
             new_project_team.save()
     return render(request, 'view_assignments.html', {'member': get_user(request), 'list': TeamProject.objects.filter(space=space), 'space': space})
